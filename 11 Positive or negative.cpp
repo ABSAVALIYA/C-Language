cecda6f8a0ff5420a239cdf9fdeeabe8206826bd
ABSAVALIYA <145214365+ABSAVALIYA@users.noreply.github.com>
2023-09-16 21:58:10 +0530

    Add files via upload

---
+++ 11 Positive or negative.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int N;
+	
+	printf("Enter number N: ");
+	scanf("%d", &N);
+	 if(N%2==0)
+	 {
+	 	printf("N is even number");
+	 }
+	 else
+	 {
+	 	printf("N is odd number");	
+	 }
+}
