db7602472333a2db0a1ce7f8ecc3191213e28576
ABSAVALIYA <145214365+ABSAVALIYA@users.noreply.github.com>
2023-09-18 15:44:49 +0530

    Add files via upload

---
+++ pr12 Odd even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int N;
+	printf("Enter a number: ");
+	scanf("%d", &N);
+	if(N%2!=0)
+	{
+		printf("Number is odd");
+	}
+	else
+	{
+		printf("Number is even");
+	}
+	
+}
