f95cd60f382beb081279a7b84abc40c8e3a4375f
ABSAVALIYA <145214365+ABSAVALIYA@users.noreply.github.com>
2023-09-25 16:05:07 +0530

    Add files via upload

---
+++ pr18 Multiply and divide by bitwise operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int N, M, D;
+	printf("Enter a number: ");
+	scanf("%d", &N);
+	
+	M = N << 1;
+	D=N>>1;
+	
+	printf("Multiplication : %d\n", M);
+	printf("Divison        : %d", D);
+	
+}
