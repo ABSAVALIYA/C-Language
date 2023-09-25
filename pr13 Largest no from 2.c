1fcf0c5d29fae50d17be8e4670e650a80c798dcd
ABSAVALIYA <145214365+ABSAVALIYA@users.noreply.github.com>
2023-09-25 15:14:52 +0530

    Add files via upload

---
+++ pr13 Largest no from 2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int A,B;
+	printf("Enter A: ");
+	scanf("%d", &A);
+	printf("Enter B: ");
+	scanf("%d", &B);
+	
+	if(A>B)
+	{
+	printf("A is Largest");
+	}
+	else
+	{
+	printf("B is largest");
+	}
+	
+}
